3c377cefdc0b39ef1249cb6741d3892c802fe228
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2019-07-08 14:54:14 +0200

    No need for BOM to show the issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.springframework.boot:spring-boot-gradle-plugin:1.5.18.RELEASE")
-    }
-}
-
 allprojects {
     repositories {
         mavenCentral()
@@ -18,8 +9,6 @@ subprojects {
     apply(plugin = "java-library")
 
     dependencies {
-      // Spring Boot managed dependencies
-      "implementation"(platform("org.springframework.boot:spring-boot-dependencies:1.5.18.RELEASE"))
       // Constraints
       constraints {
           "implementation"("args4j:args4j:2.33")
